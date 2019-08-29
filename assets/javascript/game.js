b1330b454cf36980c8d39eb27831d9b8dea13468
kspaffk <51271998+kspaffk@users.noreply.github.com>
2019-08-28 19:01:14 -0600

    set score attribute, add to html, create increase function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,16 @@
 var guessGame = {
-    wordList: ["gondor", "mirkwood", "mordor", "lorien", "rohan", "shire", "baraddur", "moria", "fangorn"],
+    wordList: ["sauron", "melkor", "aragorn", "strider", "gandalf", "galadriel", "legolas", "thranduil",
+        "baggins", "frodo", "samwise", "pippin", "gimli", "boromir", "faramir", "arwen", "bilbo",
+        "bombadil", "saruman", "meriadoc", "denethor", "elendil", "elrond", "celeborn", "eomer", "eowyn",
+        "rosie", "isildur", "haldir", "proudfoot", "smaug", "thorin", "theoden", "wormtongue"],
+    score: 0,
+    statusOfWord: "",
 
-    
     getRandomWord: function() {
         return this.wordList[Math.floor(Math.random() * this.wordList.length)];
-    }
+    },
+
+    increaseScore: function() {
+        return this.score++;
+    },
 }
