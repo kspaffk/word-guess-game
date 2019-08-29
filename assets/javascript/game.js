4a40829f6eb9ece7a1fbcd66daed2ab6f4a3cc8d
tworoose <51271998+kspaffk@users.noreply.github.com>
2019-08-28 19:42:36 -0600

    set vars for words and displaying underscores for guessing word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,12 +5,19 @@ var guessGame = {
         "rosie", "isildur", "haldir", "proudfoot", "smaug", "thorin", "theoden", "wormtongue"],
     score: 0,
     statusOfWord: "",
+    currentWord: "",
+    guessWord: "",
 
     getRandomWord: function() {
-        return this.wordList[Math.floor(Math.random() * this.wordList.length)];
+        this.currentWord = this.wordList[Math.floor(Math.random() * this.wordList.length)];
+        for (var i = 0; i < this.currentWord.length; i++) {
+            this.guessWord = this.guessWord + "_";
+        }
+        
     },
 
     increaseScore: function() {
-        return this.score++;
+        this.score++;
+        return this.score;
     },
 }
